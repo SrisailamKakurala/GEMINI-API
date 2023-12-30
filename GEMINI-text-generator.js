bcf059163b9c06ff2b87bbd5b7cb9efb8c4bcf03
srisailamkakurala <srisailamkakurala@gmail.com>
2023-12-30 20:21:02 +0530

    initial commit

--- GEMINI-text-generator.js
+++ GEMINI-text-generator.js
@@ -1,15 +1,17 @@
+// require the secret key from .env file
 const API_KEY = require('dotenv').config();
 
+// importing GoogleGenerativeAI from @google/generative-ai package
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 
-// Access your API key as an environment variable (see "Set up your API key" above)
+// Access your API key as an environment variable
 const genAI = new GoogleGenerativeAI(process.env.API_KEY);
 
 async function run() {
   // For text-only input, use the gemini-pro model
   const model = genAI.getGenerativeModel({ model: "gemini-pro"});
 
-  const prompt = "Write a story about a magic backpack."
+  const prompt = "explain about chatGPT"
 
   const result = await model.generateContent(prompt);
   const response = await result.response;
